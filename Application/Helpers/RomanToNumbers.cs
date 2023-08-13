cba7562e8107a221d667a343eedefe1b4de3bbe5
Michal Nowak <57130615+Michal-Sob@users.noreply.github.com>
2023-08-13 23:14:05 +0200

    Roman To numbers

---
+++ Application/Helpers/RomanToNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibrusScheduleHelper.Helpers;
+
+public class RomanToNumbers
+{
+    public int RomanToInt(string s)
+    {
+        var sum = 0;
+        var romanNumbersDict = new Dictionary<char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+        for (var i = 0; i < s.Length; i++)
+        {
+            var currentRomanChar = s[i];
+
+            romanNumbersDict.TryGetValue(currentRomanChar, out var num);
+
+            if (num == 0)
+                continue;
+            
+            if (i + 1 < s.Length && romanNumbersDict.ContainsKey(s[i + 1]) && romanNumbersDict[s[i + 1]] > romanNumbersDict[currentRomanChar])
+            {
+                sum -= num;
+            }
+            else
+            {
+                sum += num;
+            }
+        }
+        return sum;
+    }
+}
