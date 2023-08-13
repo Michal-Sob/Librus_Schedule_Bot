23a51cb225c716d5fd7960ba2fac80a6a8054c74
Michal Nowak <57130615+Michal-Sob@users.noreply.github.com>
2023-08-13 23:14:40 +0200

    New Curriculum added and redid model

---
+++ Core/Curriculum.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public class Curriculum
+    {
+        //public Dictionary<string, int> Values { get; set; }
+        //public List<int> numbers { get; set; }
+        public string Subject { get; set; }
+        public int[] Points { get; set; }
+        public int One { get; set; }
+
+        public int Two { get; set; }
+
+        public int Three { get; set; }
+        public int Four { get; set; }
+        public int Five { get; set; }
+        public int Six { get; set; }
+        public int Seven { get; set; }
+        public int Eight { get; set; }
+        public int Nine { get; set; }
+        public int Ten { get; set; }
+        public int Eleven { get; set; }
+        public int Twelve { get; set; }
+        public int Thirteen { get; set; }
+        public int Fourteen { get; set; }
+    }
+}
